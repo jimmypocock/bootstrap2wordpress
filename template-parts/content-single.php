b7ea066d79aa15e0ea3254729364bca206af689c
Jimmy Pocock <jimmypocock@yahoo.com>
2015-10-09 17:10:59 -0500

    Single blog post page is working like a charm.

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -11,25 +11,40 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    <?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
 
-		<div class="entry-meta">
-			<?php bootstrap2wordpress_posted_on(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+    <?php if ( 'post' === get_post_type() ) : ?>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bootstrap2wordpress' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+      <div class="post-details">
+        <i class="fa fa-user"></i> <?php the_author(); ?>
+        <i class="fa fa-clock-o"></i> <time><?php the_date(); ?></time>
 
-	<footer class="entry-footer">
-		<?php bootstrap2wordpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+        <i class="fa fa-folder"></i> <?php the_category(', '); ?>
+        <i class="fa fa-tags"></i> <?php the_tags(); ?>
+
+        <div class="post-comments-badge">
+          <a href="<?php comments_link(); ?>"><i class="fa fa-comments"></i> <?php comments_number(0, 1, '%'); ?></a>
+        </div><!-- post-comments-badge -->
+
+        <?php edit_post_link( 'Edit', '<div><i class="fa fa-pencil"></i>', '</div>' ); ?>
+
+      </div><!-- post-details -->
+
+    <?php endif; ?>
+  </header><!-- .entry-header -->
+
+	<?php if ( has_post_thumbnail() ) : ?>
+    <div class="post-image">
+      <?php the_post_thumbnail(); ?>
+    </div><!-- post-image -->
+  <?php endif; ?>
+
+  <div class="post-body">
+    <?php the_content(); ?>
+  </div><!-- post-body -->
+
+  <footer class="entry-footer">
+    <?php bootstrap2wordpress_entry_footer(); ?>
+  </footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
